277a59047b751f5dc81a276e37bdd996de7772a7
MortezaChinahkash <sascha.chinahkash@gmail.com>
2025-03-17 11:17:19 +0100

    put the first dishes into de db.js

---
+++ db.js
@@ -0,0 +1,44 @@
+let dishes = [
+    {
+        "name": "Filetto di Manzo al Tartufo",
+        "description": "Saftiges Rinderfilet (250g) mit schwarzer Trüffelsauce, Parmesanspänen und Rosmarinkartoffeln.",
+        "price": 38.90 
+    },
+    {
+        "name": "Tagliatelle al Tartufo e Gamberi",
+        "description": "Frische Tagliatelle in cremiger Trüffelsauce mit Riesengarnelen, Parmesan und Rucola.",
+        "price": 27.50 
+    },
+    {
+        "name": "Pizza Gourmet Prosciutto e Fichi",
+        "description": "Hausgemachter Pizzateig mit Burrata, Parma-Schinken, frischen Feigen und einem Hauch Honig.",
+        "price": 24.90
+    },
+    {
+        "name": "Risotto al Limone e Capesante",
+        "description": "Cremiges Zitronen-Risotto mit Jakobsmuscheln, Safran und gehobeltem Pecorino.",
+        "price": 29.00 
+    },
+    {
+        "name": "Branzino al Forno con Verdure",
+        "description": "Ofengegarter Wolfsbarsch mit mediterranem Gemüse, Zitronenbutter und einem Hauch von Meersalz.",
+        "price": 32.90 
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
