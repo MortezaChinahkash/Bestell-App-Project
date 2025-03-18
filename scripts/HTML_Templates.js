5b132c84874237d1f238998e190966d36f402685
MortezaChinahkash <sascha.chinahkash@gmail.com>
2025-03-18 01:13:24 +0100

    function_template

---
+++ scripts/HTML_Templates.js
@@ -0,0 +1,48 @@
+function renderDishesHTMLTemplate(i, priceRefReplace){
+    return  `
+    <div class="dish_selection margin_lr">
+        <div class="title_addbtn">
+            <h3 class="dish_title">${dishes[i].name}</h3>
+            <img onclick="addToCart(${i})" class="add_button" src="./assets/png/add_2_24dp_FF8000_FILL0_wght400_GRAD0_opsz24.png">
+        </div>
+        <p class="dish_text margin_lr">${dishes[i].description}</p>
+        <p class="dish_price margin_lr">${priceRefReplace}€</p>
+    </div>
+    `;
+}
+function renderSideDishesHTMLTemplate(i, priceRefReplace){
+    return  `
+    <div class="dish_selection margin_lr">
+        <div class="title_addbtn">
+            <h3 class="dish_title">${dishes[i].name}</h3>
+            <img onclick="addToCart(${i})" class="add_button" src="./assets/png/add_2_24dp_FF8000_FILL0_wght400_GRAD0_opsz24.png">
+        </div>
+        <p class="dish_text margin_lr">${dishes[i].description}</p>
+        <p class="dish_price margin_lr">${priceRefReplace}€</p>
+    </div>
+    `;
+}
+function printCartHTMLTemplate(i, calculatedPriceReplace){
+    return  `
+    <h3 class="margin_lr">${dishes[i].name}</h3>
+    <div class="price_amount margin_lr">
+        <div class="amount">
+            <img class="add_rmv_btn" onclick="removeOneDish(${i})" src="./assets/png/remove_24dp_FF8000_FILL0_wght400_GRAD0_opsz24.png">
+            <p class="margin_lr">${dishes[i].amount}x</p>
+            <img class="add_rmv_btn" onclick="addToCart(${i})" src="./assets/png/add_2_24dp_FF8000_FILL0_wght400_GRAD0_opsz24.png">
+        </div>
+        <div class="price">
+            <p id="calculateTotal${i}">${calculatedPriceReplace}</p><p>€</p>
+            <img class="add_rmv_btn" onclick="deleteDishFromCart(${i})" src="./assets/png/delete_24dp_FF8000_FILL0_wght400_GRAD0_opsz24.png">
+        </div>
+    </div>
+    `;
+}
+function totalSumHTMLTemplate(totalPriceReplace, totalWDeliveryReplace){
+    return `
+      <hr>
+      <p class="checkout_summary">Zwischensumme: ${totalPriceReplace}€</p>
+      <p class="checkout_summary">Lieferkosten: 5,00€ </p>
+      <p class="checkout_summary_total">Gesamt: ${totalWDeliveryReplace}€</p>
+      `;
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,3 @@
-
 function renderDishes() {
     getFromLocalStorage();
   for (let i = 0; i < 5; i++) {
@@ -6,34 +5,19 @@ function renderDishes() {
     let priceRefString = priceRef.toString()
     let priceRefReplace = priceRefString.replace(".", ",")
     let maincourseRef = document.getElementById("maincourseEntry");
-    maincourseRef.innerHTML += /*html*/ `
-        <div class="dish_selection margin_lr">
-            <div class="title_addbtn">
-                <h3 class="dish_title">${dishes[i].name}</h3>
-                <img onclick="addToCart(${i})" class="add_button" src="./assets/png/add_2_24dp_FF8000_FILL0_wght400_GRAD0_opsz24.png">
-            </div>
-            <p class="dish_text margin_lr">${dishes[i].description}</p>
-            <p class="dish_price margin_lr">${priceRefReplace}€</p>
-        </div>
-        `;
+    maincourseRef.innerHTML += renderDishesHTMLTemplate(i, priceRefReplace)
   }
+    renderSideDishes()
+}
+
+function renderSideDishes(){
   for (let i = 5; i < 10; i++) {
     let priceRef = dishes[i].price.toFixed(2);
     let priceRefString = priceRef.toString()
     let priceRefReplace = priceRefString.replace(".", ",")
     let sidecourseRef = document.getElementById("sidecourseEntry");
-    sidecourseRef.innerHTML += /*html*/ `
-        <div class="dish_selection margin_lr">
-            <div class="title_addbtn">
-                <h3 class="dish_title">${dishes[i].name}</h3>
-                <img onclick="addToCart(${i})" class="add_button" src="./assets/png/add_2_24dp_FF8000_FILL0_wght400_GRAD0_opsz24.png">
-            </div>
-            <p class="dish_text margin_lr">${dishes[i].description}</p>
-            <p class="dish_price margin_lr">${priceRefReplace}€</p>
-        </div>
-        `;
+    sidecourseRef.innerHTML += renderSideDishesHTMLTemplate(i, priceRefReplace)
   }
-
 }
 
 function printCart() {
@@ -50,24 +34,10 @@ function printCart() {
         let calculatedPriceToFixed = calculatedPrice.toFixed(2)
         let calculatedPriceString = calculatedPriceToFixed.toString()
         let calculatedPriceReplace = calculatedPriceString.replace(".", ",")
-        cartEntry.innerHTML += /*html*/ `
-        <h3 class="margin_lr">${dishes[i].name}</h3>
-        <div class="price_amount margin_lr">
-            <div class="amount">
-                <img class="add_rmv_btn" onclick="removeOneDish(${i})" src="./assets/png/remove_24dp_FF8000_FILL0_wght400_GRAD0_opsz24.png">
-                <p class="margin_lr">${dishes[i].amount}x</p>
-                <img class="add_rmv_btn" onclick="addToCart(${i})" src="./assets/png/add_2_24dp_FF8000_FILL0_wght400_GRAD0_opsz24.png">
-            </div>
-            <div class="price">
-                <p id="calculateTotal${i}">${calculatedPriceReplace}</p><p>€</p>
-                <img class="add_rmv_btn" onclick="deleteDishFromCart(${i})" src="./assets/png/delete_24dp_FF8000_FILL0_wght400_GRAD0_opsz24.png">
-            </div>
-        </div>
-        `;
+        cartEntry.innerHTML +=printCartHTMLTemplate(i, calculatedPriceReplace)
       }
     }
   
-
     if (!isCartEmpty) {
       let totalWDelivery = totalPrice + 5
       let totalWDeliveryFixed = totalWDelivery.toFixed(2) 
@@ -76,12 +46,7 @@ function printCart() {
       let totalPriceFixed = totalPrice.toFixed(2)
       let totalPriceString = totalPriceFixed.toString()
       let totalPriceReplace = totalPriceString.replace(".",",")
-      totalPriceContainer.innerHTML = `
-      <hr>
-      <p class="checkout_summary">Zwischensumme: ${totalPriceReplace}€</p>
-      <p class="checkout_summary">Lieferkosten: 5,00€ </p>
-      <p class="checkout_summary_total">Gesamt: ${totalWDeliveryReplace}€</p>
-      `;
+      totalPriceContainer.innerHTML = totalSumHTMLTemplate(totalPriceReplace, totalWDeliveryReplace) 
     } else {
       totalPriceContainer.innerHTML = "";
     }
